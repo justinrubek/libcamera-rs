29707c0d4e1e25ad007f1813f59a4913d89636f9
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-07-06 18:05:05 -0500

    fix(nix): libcamera environment variables

--- flake-parts/shells.nix
+++ flake-parts/shells.nix
@@ -25,6 +25,8 @@
         LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath packages;
         LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
         RUST_SRC_PATH = "${self'.packages.rust-toolchain}/lib/rustlib/src/rust/src";
+        LIBCAMERA_IPA_PROXY_PATH = "${inputs'.libcamera.packages.libcamera}/libexec/libcamera";
+        LIBPISP_BE_CONFIG_FILE = "${inputs'.libcamera.packages.libpisp}/share/libpisp/backend_default_config.json";
       };
     };
   };
